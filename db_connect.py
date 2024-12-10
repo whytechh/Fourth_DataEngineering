e79f0e3aa546ebf5481c44d98f0e1b699ef7975a
whytechh <whytechh@yandex.com>
2024-12-10 23:08:43 +0500

    Update db_connect.py

--- db_connect.py
+++ db_connect.py
@@ -1,6 +1,6 @@
 import sqlite3
 
 def connect_to_database():
-    connect = sqlite3.connect(r'lab_4\tasks.db')
+    connect = sqlite3.connect('tasks.db')
     connect.row_factory = sqlite3.Row
     return connect
